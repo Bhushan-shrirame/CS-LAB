973dd430c5225d1203f73866d860e106af47a670
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2023-08-01 08:31:29 +0000

    some fix

--- 27-07-2023/Problem1.cpp
+++ 27-07-2023/Problem1.cpp
@@ -4,11 +4,10 @@ using namespace std;
 // A recursive function to find nth catalan number
 unsigned long int catalan(unsigned int n)
 {
-	// Base case
+
 	if (n <= 1)
 		return 1;
 
-	// catalan(n) is sum of
 	// catalan(i)*catalan(n-i-1)
 	unsigned long int res = 0;
 	for (int i = 0; i < n; i++)
@@ -17,7 +16,6 @@ unsigned long int catalan(unsigned int n)
 	return res;
 }
 
-// Driver code
 int main()
 {
     int i;
