f18d9614a4bfd333aa063534c283b576b1a6270b
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2023-08-21 13:42:11 +0530

    que 1

--- Assigments-4/Problem1.cpp
+++ Assigments-4/Problem1.cpp
@@ -1,43 +1,69 @@
 #include <bits/stdc++.h>
-using namespace std;
-
-int dfs(int node, int par, bool isBlack,
-		vector<vector<int> >& adj)
+struct node
 {
-	int no_Of_Black = isBlack;
-	for (int i : adj[node]) {
-		if (i == par)
-			continue;
-
-		no_Of_Black
-			+= dfs(i, node, !isBlack, adj);
-	}
-	return no_Of_Black;
-}
-
-long long maxEdges(int n,
-				vector<pair<int, int> > edges)
-{
-	vector<vector<int> > adj(n + 1);
-	for (auto i : edges) {
-		adj[i.first].push_back(i.second);
-		adj[i.second].push_back(i.first);
-	}
-
-	int no_Of_Black = dfs(1, 0, 1, adj);
-
-	int no_Of_White = n - no_Of_Black;
-
-	return (1LL * (no_Of_Black)
-				* (no_Of_White)
-			- (n - 1));
-}
+    node *parent = NULL;
+    int level = -1;
+    std::vector<node *> adj;
+};
 
 int main()
 {
-	int N = 4;
-	vector<pair<int, int> > edges
-		= { { 1, 2 }, { 2, 3 }, { 1, 4 } };
-	cout << maxEdges(N, edges) << endl;
-	return 0;
+    int e;
+    int n;
+    std::cin >> n;
+    std::cin >> e;
+    std::vector<node *> arr(n + 1);
+    for (int i = 0; i < n; i++)
+    {
+        node *t = new node();
+        arr[i] = t;
+    }
+    for (int i = 0; i < e; i++)
+    {
+        int n1, n2;
+        std::cin >> n1;
+        std::cin >> n2;
+        n1 = n1 - 1;
+        n2 = n2 - 1;
+        arr[n1]->adj.push_back(arr[n2]);
+        arr[n2]->parent = arr[n1];
+    }
+    node *root_node;
+    for (int i = 0; i < n; i++)
+    {
+        if (!arr[i]->parent)
+            root_node = arr[i];
+    }
+    int ll = 0;
+    std::vector<std::vector<node *>> levels;
+    std::vector<node *> t;
+    t.push_back(root_node);
+    levels.push_back(t);
+    std::cout << levels.size();
+    while (true)
+    {
+        std::vector<node *> nl;
+        for (int i = 0; i < levels[ll].size(); i++)
+        {
+            for (int j = 0; j < levels[ll][i]->adj.size(); j++)
+            {
+                nl.push_back(levels[ll][i]->adj[j]);
+            }
+        }
+        if (nl.size() == 0)
+            break;
+        levels.push_back(nl);
+        ++ll;
+    }
+    int odd_node = 0, even_node = 0;
+    for (int i = 0; i < levels.size(); i++)
+    {
+        if (i & 1)
+        {
+            odd_node += levels[i].size();
+        }
+        else
+            even_node += levels[i].size();
+    }
+    std::cout << (odd_node * even_node) - e;
 }
