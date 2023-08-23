2ac09f1d5b9b6f92dacf73d1a5fb5b0cd314c32c
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2023-08-23 17:37:24 +0000

    question 4 changes

--- Assigments-4/Problem4.cpp
+++ Assigments-4/Problem4.cpp
@@ -9,29 +9,40 @@ int main(){
         cin >> arr[i];
     }
     
-    int peak=0, bottom1 = arr[0], bottom2=0 , mls=0, pre=0;
-   
-    for(int i=0;i <n; i++){
-       if(arr[i] == arr[i+1]){
-           bottom1 = i+1;
-           peak =0;
-       } 
-       if(arr[i] > arr[i-1] && arr[i] > arr[i+1]){
-        peak = i;
-       }
-       if(arr[i] < arr[i+1] && peak){
-          bottom2 = i;
-          mls = bottom2 - bottom1 + 1;
-          bottom1 = i;
-          peak = 0;
-       }
+    int peak=0, bottom1 = 0, bottom2=0 , mls=0, maxmls = 0;
        
-       if( pre < mls) pre= mls;
+       if(arr[0] >= arr[1]){
+        bottom1 = 1;
+       }
 
-       
+    for(int i=1;i <n-1; i++){
+        if((arr[i] >= arr[i-1] ) && (arr[i] >= arr[i+1])){
+            peak = i;
+        }
+        if(arr[i] <= arr[i+1] && peak){
+            bottom2 = i;
+            mls = bottom2 - bottom1 + 1;
+             if(arr[i] < arr[i+1]){
+                bottom1 = i;
+             }
+             if(arr[i] == arr[i+1]){
+                bottom1 = i+1; 
+             }
+            peak = 0;
+        }
+        if(maxmls < mls){
+            maxmls = mls;
+        }
     }
-       if(pre <=2 ){
+    if (bottom2 < peak){
+        bottom2 = n-1;
+        mls = bottom2 - bottom1 + 1;
+        if(maxmls < mls){
+            maxmls = mls;
+        }
+    }
+       if(maxmls <=2 ){
         return 0;
        }
-    cout << "The maximum length of a subsequence of the array is "<<  pre << endl;
+    cout << "The maximum length of a subsequence of the array is "<<  maxmls << endl;
 }
