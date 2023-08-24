41b0900ebb44e387c628376a9f9b692b4434a5ec
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2023-08-24 06:20:26 +0000

    .

--- Assigments-5/Problem2.cpp
+++ Assigments-5/Problem2.cpp
@@ -24,5 +24,3 @@ int main()
     int res = maxUnique(S , st) ;
     cout<<res << endl;
 }
-
-
--- Assigments-5/Problem5.cpp
+++ Assigments-5/Problem5.cpp
@@ -1,2 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+
+
+int findsum(){
+    int sum =0;
+    return sum;
+}
+
+int main(){
+    int N , M;
+    cin >> N >> M;
+     int arr[N][M];
+     for(int i=0; i <N ;i++){
+        for(int j=0;j<M;j++){
+            cin >> arr[i][j];
+        }
+     }
+
+     int value[N][M];
+
+     for(int i=0; i <N ;i++){
+        for(int j=0;j<M;j++){
+            if(i-1 >= 0){
+                value[i][j] = 0;
+            }
+        }
+     }
+
+
+}
