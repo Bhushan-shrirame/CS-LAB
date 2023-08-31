cd7ce464e6c3d9642871cf225ec9152a97b003dc
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2023-08-31 05:09:43 +0000

    problem 3 completed

---
+++ Assigments-6/Problem3.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int D,W;
+    cin >> D >> W;
+    int difficulty[D],profit[D],worker[W];  
+    for(int i=0; i <D;i++){
+        cin >> difficulty[i];
+    }
+    for(int i=0; i <D;i++){
+        cin >> profit[i];
+    }
+    for(int i=0; i <W;i++){
+        cin >> worker[i];
+    }
+    
+
+    int maxprofit =0;
+    for(int i=0;i<W;i++){
+        int tempprofit =0;
+        for(int j=0; j<D ;j++){
+            if(worker[i] >= difficulty[j]){
+                if(tempprofit < profit[j]){
+                    tempprofit = profit[j];
+                    
+                }
+            }
+        }
+
+        maxprofit += tempprofit;
+    }
+
+
+    cout << " the maximum profit we can achieve after assigning the workers to the jobs is  " << maxprofit << endl;
+}
