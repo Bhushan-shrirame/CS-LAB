5880fd1c0510e2f1d10d2b77de446e13ee10e5f6
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2023-09-04 14:51:02 +0000

    allover

--- Assigments-6/Problem1.cpp
+++ Assigments-6/Problem1.cpp
@@ -1,2 +1,87 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+
 using namespace std;
+
+bool backtrack(vector<int>& nums, vector<vector<int>>& subsets, unordered_map<int, int>& count, int targetSum, int K, int currentSum, int startIndex) {
+    if (K == 0) {
+        return true;
+    }
+
+    if (currentSum == targetSum) {
+        return backtrack(nums, subsets, count, targetSum, K - 1, 0, 0);
+    }
+
+    for (int i = startIndex; i < nums.size(); i++) {
+        if (count[nums[i]] > 0 && currentSum + nums[i] <= targetSum) {
+            count[nums[i]]--;
+            subsets[K - 1].push_back(nums[i]);
+
+            if (backtrack(nums, subsets, count, targetSum, K, currentSum + nums[i], i + 1)) {
+                return true;
+            }
+
+            count[nums[i]]++;
+            subsets[K - 1].pop_back();
+        }
+    }
+
+    return false;
+}
+
+vector<vector<int>> canPartitionKSubsets(vector<int>& nums, int K) {
+    int totalSum = 0;
+    for (int num : nums) {
+        totalSum += num;
+    }
+
+    if (totalSum % K != 0) {
+        return {};
+    }
+
+    int targetSum = totalSum / K;
+    unordered_map<int, int> count;
+    vector<vector<int>> subsets(K, vector<int>());
+    
+    for (int num : nums) {
+        count[num]++;
+    }
+
+    bool canPartition = backtrack(nums, subsets, count, targetSum, K, 0, 0);
+
+    return canPartition ? subsets : vector<vector<int>>();
+}
+
+int main() {
+	int n , a;
+	cin >> n;
+
+    vector<int> nums;
+    int K = 4;
+	for(int i=0 ; i<n ; i++)
+    {
+        cin>>a;
+        nums.push_back(a);
+    }
+
+    vector<vector<int>> result = canPartitionKSubsets(nums, K);
+
+    if (result.empty()) {
+        cout << "It is not possible to divide the array into " << K << " subsets with the same sum." << endl;
+    } else {
+        cout << "Possible subsets with the same sum:" << endl;
+        for (const vector<int>& subset : result) {
+            for (int num : subset) {
+                cout << num << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    return 0;
+	
+}
+//  4 3 2 3 5 2 1
+
--- Assigments-6/Problem2.cpp
+++ Assigments-6/Problem2.cpp
@@ -1,2 +1,105 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+
+void constructResults(const string& startWord, const string& currentWord, const unordered_map<string, vector<string>>& parentMap, vector<string>& path, vector<vector<string>>& results) {
+    if (currentWord == startWord) {
+        path.push_back(startWord);
+        vector<string> temp = path;
+        reverse(temp.begin(), temp.end()); // Reverse the path to get the correct order
+        results.push_back(temp);
+        path.pop_back();
+        return;
+    }
+    
+    path.push_back(currentWord);
+    
+    for (const string& parent : parentMap.at(currentWord)) {
+        constructResults(startWord, parent, parentMap, path, results);
+    }
+    
+    path.pop_back();
+}
+vector<vector<string>> findLadders(string startWord, string endWord, vector<string>& wordList) {
+    unordered_set<string> wordSet(wordList.begin(), wordList.end());
+    vector<vector<string>> results;
+    
+    if (wordSet.find(endWord) == wordSet.end()) {
+        return results; // endWord is not in the wordList, no transformation possible
+    }
+    
+    queue<string> bfsQueue;
+    bfsQueue.push(startWord);
+    
+    unordered_map<string, vector<string>> parentMap;
+    parentMap[startWord] = vector<string>();
+    
+    bool foundEndWord = false;
+    
+    while (!bfsQueue.empty() && !foundEndWord) {
+        int currentSize = bfsQueue.size();
+        unordered_set<string> currentLevelWords;
+        
+        for (int i = 0; i < currentSize; i++) {
+            string word = bfsQueue.front();
+            bfsQueue.pop();
+            
+            for (int j = 0; j < word.length(); j++) {
+                string newWord = word;
+                for (char ch = 'a'; ch <= 'z'; ch++) {
+                    newWord[j] = ch;
+                    
+                    if (wordSet.find(newWord) != wordSet.end()) {
+                        if (newWord == endWord) {
+                            foundEndWord = true;
+                        }
+                        
+                        if (parentMap.find(newWord) == parentMap.end()) {
+                            bfsQueue.push(newWord);
+                            currentLevelWords.insert(newWord);
+                            parentMap[newWord] = {word};
+                        } else {
+                            parentMap[newWord].push_back(word);
+                        }
+                    }
+                }
+            }
+        }
+        
+        // Remove words at the current level from wordSet to avoid revisiting them
+        for (const string& word : currentLevelWords) {
+            wordSet.erase(word);
+        }
+    }
+    
+    if (foundEndWord) {
+        vector<string> path;
+        constructResults(startWord, endWord, parentMap, path, results);
+    }
+    
+    return results;
+}
+
+
+
+int main() {
+    string startWord = "hit";
+    string endWord = "cog";
+    vector<string> wordList = {"hot", "dot", "dog", "lot", "log", "cog"};
+    
+    vector<vector<string>> sequences = findLadders(startWord, endWord, wordList);
+    
+    if (sequences.empty()) {
+        cout << "No transformation sequences found." << endl;
+    } else {
+        cout << "Shortest transformation sequences:" << endl;
+        for (const vector<string>& sequence : sequences) {
+            for (const string& word : sequence) {
+                cout << word << " -> ";
+            }
+            cout << endl;
+        }
+    }
+    
+    return 0;
+}
