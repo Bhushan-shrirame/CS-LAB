6e8edab21cc971ad18f87c22ba3d528e084fec17
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2023-09-24 05:26:56 +0000

    probem1

--- Assigments-7/Problem1.cpp
+++ Assigments-7/Problem1.cpp
@@ -1,2 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+int numDecodings(string s) {
+    if (s.empty() || s[0] == '0') {
+        return 0;
+    }
+    
+    int n = s.size();
+    vector<int> dp(n + 1, 0);
+    
+    dp[0] = 1;
+    dp[1] = 1;
+    
+    for (int i = 2; i <= n; ++i) {
+        if (s[i - 1] != '0') {
+            dp[i] += dp[i - 1];
+        }
+        
+        int twoDigit = stoi(s.substr(i - 2, 2));
+        if (twoDigit >= 10 && twoDigit <= 26) {
+            dp[i] += dp[i - 2];
+        }
+    }
+    
+    return dp[n];
+}
+
+int main() {
+    string encoded_message ;
+    cin >> encoded_message;
+    int ways = numDecodings(encoded_message);
+    cout << "Number of ways to decode: " << ways << endl;
+    return 0;
+}
--- Assigments-7/Problem4.cpp
+++ Assigments-7/Problem4.cpp
@@ -1,2 +1,80 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+// Structure to store row and column information
+struct RowColInfo {
+    int value;
+    int row;
+    int col;
+};
+
+// Function to construct M'
+vector<vector<int>> constructMatrixM(vector<vector<int>>& M) {
+    int n = M.size();
+    
+    // Initialize row and column arrays
+    vector<RowColInfo> rowInfo(n);
+    vector<RowColInfo> colInfo(n);
+    
+    for (int i = 0; i < n; ++i) {
+        rowInfo[i].value = *min_element(M[i].begin(), M[i].end());
+        rowInfo[i].row = i;
+    }
+    
+    for (int j = 0; j < n; ++j) {
+        vector<int> colValues;
+        for (int i = 0; i < n; ++i) {
+            colValues.push_back(M[i][j]);
+        }
+        auto maxElem = max_element(colValues.begin(), colValues.end());
+        colInfo[j].value = *maxElem;
+        colInfo[j].col = j;
+    }
+    
+    // Sort row and column arrays
+    sort(rowInfo.begin(), rowInfo.end(), [](const RowColInfo& a, const RowColInfo& b) {
+        return a.value < b.value;
+    });
+    
+    sort(colInfo.begin(), colInfo.end(), [](const RowColInfo& a, const RowColInfo& b) {
+        return a.value z b.value;
+    });
+    
+    // Construct M' based on sorted arrays
+    vector<vector<int>> M_prime(n, vector<int>(n, 0));
+    
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            M_prime[rowInfo[i].row][colInfo[j].col] = M[i][j];
+        }
+    }
+    
+    return M_prime;
+}
+
+int main() {
+    int n;
+    cout << "Enter the size of the matrix M: ";
+    cin >> n;
+    
+    vector<vector<int>> M(n, vector<int>(n));
+    
+    cout << "Enter the elements of the matrix M:" << endl;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cin >> M[i][j];
+        }
+    }
+    
+    vector<vector<int>> M_prime = constructMatrixM(M);
+    
+    cout << "Matrix M':" << endl;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cout << M_prime[i][j] << " ";
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
