4a4a72c9bbcab7fdb6cbd6b5f4d1e88ae47be108
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2023-09-25 06:33:27 +0000

    .

--- Assigments-7/Problem2.cpp
+++ Assigments-7/Problem2.cpp
@@ -26,8 +26,6 @@ int maxinumsorted(int arrus[], int N , int M){
     return count;
 }
 
-
-
 int main(){
     int N,M;
     cin >> N;
@@ -43,28 +41,7 @@ int main(){
 
     sort(arrs,arrs +N);
 
-    cout << "maximum number of elements in sorted is" << maxinsorted(arrs ,N ,M) << endl;
-    cout << "maximum number of elements in unsorted is " << maxinumsorted(arrus ,N ,M) << endl;
-
-    return 0;
-}
-
-int main(){
-    int N,M;
-    cin >> N;
-    cin >> M;
-
-    int arrs[N],arrus[N];
-    
-    for(int i=0; i<N;i++){
-        cin >> arrs[i];
-        arrus[i] = arrs[i];
-    }
-    
-
-    sort(arrs,arrs +N);
-
-    cout << "maximum number of elements in sorted is" << maxinsorted(arrs ,N ,M) << endl;
+    cout << "maximum number of elements in sorted is " << maxinsorted(arrs ,N ,M) << endl;
     cout << "maximum number of elements in unsorted is " << maxinumsorted(arrus ,N ,M) << endl;
 
     return 0;
