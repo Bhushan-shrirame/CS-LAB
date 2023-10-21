88313f6c1a4f67993c67d103b2af6dd3fa4e2854
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2023-10-21 06:09:51 +0000

    .

--- Assigments-9/Problem3.cpp
+++ Assigments-9/Problem3.cpp
@@ -1,10 +1,47 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+
 using namespace std;
 
-int main(){
+int main() {
+    int N, M, K;
+    cin >> N >> M >> K;
 
+    map<int, int> permits;
+    for (int i = 0; i < K; i++) {
+        int X, Y;
+        cin >> X >> Y;
+        permits[Y] = X;  // Store the permitted truck for each road
+    }
 
+    vector<int> capacity(M);
+    for (int i = 0; i < M; i++) {
+        cin >> capacity[i];
+    }
 
-    
-    return 0
+    int Z;
+    cin >> Z;
+
+    for (int i = 0; i < Z; i++) {
+        int R, P;
+        cin >> R >> P;
+
+        int max_trucks = 0;
+        capacity[R - 1] -= P;
+
+        // Check the permitted truck for road R
+        int truck_on_road = permits[R];
+
+        if (truck_on_road <= capacity[R - 1]) {
+            max_trucks = truck_on_road;
+        } else {
+            max_trucks = capacity[R - 1];
+        }
+
+        cout << max_trucks << endl;
+    }
+
+    return 0;
 }
