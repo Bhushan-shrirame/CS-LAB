c3e6a79a206afc34a1867777dc0d7b9fceb5f1bb
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2023-10-24 15:11:24 +0000

    .

---
+++ Assigments-10/Problem1.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    void sovle(){
+        
+    }
+ 
+
+};
+
+int main(){
+    Solution sol;
+    sol.sovle();
+    return 0;
+}
