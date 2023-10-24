fe1b49f9bc1aa51ea4ba508724947289fc1a9ea3
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2023-10-24 15:11:59 +0000

    .

--- Assigments-10/Problem1.cpp
+++ Assigments-10/Problem1.cpp
@@ -5,7 +5,7 @@ using namespace std;
 class Solution {
 public:
     void sovle(){
-        
+        // Write your code here
     }
  
 
@@ -13,6 +13,7 @@ public:
 
 int main(){
     Solution sol;
+    // Update the solve function as per the requirement
     sol.sovle();
     return 0;
 }
